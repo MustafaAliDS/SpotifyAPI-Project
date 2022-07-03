ef8cf437acbab0734af3e86ff5702ab6f9d0aac3
MustafaAliDS <dsalimustafa@gmail.com>
2022-07-02 20:37:40 -0700

    chore: accespted merges

--- src/server.ts
+++ src/server.ts
@@ -1,40 +1,47 @@
 import express, { Application, Request, Response } from "express";
 import querystring from "querystring";
 import request from "request";
+import dotenv from "dotenv";
+dotenv.config();
 
-const app: Application = express();
-const PORT: Number = 8888;
-let client_id: string = "";
-let client_secret: string = "";
-let response_type: string = "code";
+const APP: Application = express();
+const PORT = process.env["PORT"]
+const CLIENT_ID = process.env["CLIENT_ID"];
+const CLIENT_SECERET = process.env["CLIENT_SECERET"];
+const RESPONSE_TYPE = "code";
 
-let redirect_uri: string = "http://localhost:8888/callback";
-
-let scope: string =
+const REDIRECT_URI = "http://localhost:8888/callback";
+const SCOPE =
   "user-read-recently-played user-read-email user-read-private user-read-recently-played user-read-playback-position user-read-playback-state ugc-image-upload";
 
-app.get("/login", (req: Request, res: Response) => {
+APP.get("/", (req: Request, res: Response) => {
+  res.redirect("/login");
+  console.log(req);
+  console.log(CLIENT_ID);
+  console.log(PORT);
+  console.log(CLIENT_SECERET);
+});
+
+APP.get("/login", (req: Request, res: Response) => {
   res.redirect(
     "https://accounts.spotify.com/authorize?" +
       querystring.stringify({
-        response_type: response_type,
-        client_id: client_id,
-        scope: scope,
-        redirect_uri: redirect_uri,
+        response_type: RESPONSE_TYPE,
+        client_id: CLIENT_ID,
+        scope: SCOPE,
+        redirect_uri: REDIRECT_URI,
       })
   );
   req.body;
 });
 
-app.get("/callback", (req: Request, res: Response) => {
-  const combinedIds = client_id + ":" + client_secret;
-  const encode = (combinedIds: string): string =>
-    Buffer.from(combinedIds, "binary").toString("base64");
+APP.get("/callback", (req: Request, res: Response) => {
+  const COMBINED_IDS = `${CLIENT_ID}:${CLIENT_SECERET}`;
 
   let authOptions: {
     url: string;
     form: {
-      code: string;
+      code: typeof req.query["code"];
       redirect_uri: string;
       grant_type: string;
     };
@@ -45,36 +52,33 @@ app.get("/callback", (req: Request, res: Response) => {
   } = {
     url: "https://accounts.spotify.com/api/token",
     form: {
-      code: req.body?.code,
-      redirect_uri: redirect_uri,
+      code: req.query["code"],
+      redirect_uri: REDIRECT_URI,
       grant_type: "authorization_code",
     },
     headers: {
-      Authorization: "Basic " + encode,
+      Authorization: `Basic ${Buffer.from(COMBINED_IDS).toString("base64")}`,
     },
     json: true,
   };
 
-  request.post(
-    authOptions,
-    function (response: Response, body: any, error: string) {
-      if (!error && response.statusCode === 200) {
-        let access_token: string = body.access_token;
-        let token_type: string = body.token_type;
-        scope = body.scope;
-        let expires_in: string = body.expires_in;
+  request.post(authOptions, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      let access_token: string = body.access_token;
+      let token_type: string = body.token_type;
+      let scope: string = body.scope;
+      let expires_in: string = body.expires_in;
 
-        res.send({
-          access_token: access_token,
-          token_type: token_type,
-          scope: scope,
-          expires_in: expires_in,
-        });
-      }
+      res.send({
+        access_token: access_token,
+        token_type: token_type,
+        scope: scope,
+        expires_in: expires_in,
+      });
     }
-  );
+  });
 });
 
-app.listen(PORT, () => {
+APP.listen(PORT, () => {
   console.log(`Working on port ${PORT}`);
 });
