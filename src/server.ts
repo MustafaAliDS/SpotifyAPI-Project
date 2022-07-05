c4c67134aa19cb01b7e8d6875870abd2edb767e5
moose <dsalimustafa@gmail.com>
2022-07-04 18:51:17 -0700

    chore: fix issue that causes dotenv variables to not work

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,22 @@
 import express, { Application, Request, Response } from "express";
 import querystring from "querystring";
 import request from "request";
-import dotenv from "dotenv";
+import * as dotenv from 'dotenv'
 dotenv.config();
 
+
 export const server: Application = express();
-const PORT = 8888;
-const CLIENT_ID = "";
-const CLIENT_SECERET = "";
+
+
+const { PORT,CLIENT_ID,CLIENT_SECERET } = process.env;
+
 const RESPONSE_TYPE = "code";
 
 const REDIRECT_URI = "http://localhost:8888/callback";
 const SCOPE =
   "user-read-recently-played user-read-email user-read-private user-read-recently-played user-read-playback-position user-read-playback-state ugc-image-upload";
+
+
 
 interface Body {
   access_token: string;
@@ -39,8 +43,14 @@ server.get("/login", (req: Request, res: Response) => {
 });
 
 server.get("/callback", (req: Request, res: Response) => {
+  if(CLIENT_ID === undefined){
+    throw new Error('CLIENT_ID is undefined');
+  }
+  else if(CLIENT_SECERET === undefined){
+    throw new Error('CLIENT_SECERET is undefined');
+  }
+else{
   const COMBINED_IDS = `${CLIENT_ID}:${CLIENT_SECERET}`;
-
   const AUTH_OPTIONS: {
     url: string;
     form: {
@@ -80,10 +90,16 @@ server.get("/callback", (req: Request, res: Response) => {
       });
     }
   });
+}
 });
 
 if (process.env['NODE_ENV'] !== 'test') {
+  if(PORT === undefined){
+    throw new Error('PORT is undefined');
+  }
+  else {
   server.listen(PORT, () => {
     console.log(`Working on port ${PORT}`);
   });
+}
 }
